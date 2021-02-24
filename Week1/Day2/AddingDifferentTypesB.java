199a558ecfcf476b6cdc173c3f5a50a356c6351f
Gloria L <79482385+gloria-ss@users.noreply.github.com>
2021-02-24 13:36:30 -0500

    Add files via upload

---
+++ Week1/Day2/AddingDifferentTypesB.java
@@ -0,0 +1,34 @@
+package com.lavenberg;
+
+import java.util.Scanner;
+
+public class AddingDifferentTypesB {
+	public static void main(String args[]) {
+	//Getting input from user
+	Scanner input = new Scanner (System.in);
+	System.out.println("Enter an int, float and double number seperated by spaces: ");
+	
+	int a = input.nextInt();
+	System.out.println("You entered integer" + a);
+	
+	double b = input.nextDouble(); 
+    System.out.println("You entered double " + b); 
+    
+    float c = input.nextFloat(); 
+    System.out.println("You entered float " + c);
+    //Printing number to the screen
+    System.out.println("\n");
+    System.out.println("Types Entered...");
+    System.out.println("    integer a = " + a);
+    System.out.println("    double b = " + b);
+    System.out.println("    float c = " + c);
+
+    //Adding numbers
+    System.out.println("\n");
+    System.out.println("Adding Types...");
+    System.out.println("    int a + float b = " + (a + b));
+    System.out.println("    double b + float c = " + (b + c));
+    System.out.println("    int a + double b + float c = " + (a + b + c));
+	
+	}
+}
