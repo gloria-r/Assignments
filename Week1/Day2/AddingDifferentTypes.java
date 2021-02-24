a61d4658847bb7ea91e789b5c1427f42042966cf
Gloria L <79482385+gloria-ss@users.noreply.github.com>
2021-02-24 13:23:47 -0500

    Add files via upload

--- Week1/Day2/AddingDifferentTypes.java
+++ Week1/Day2/AddingDifferentTypes.java
@@ -1,34 +1,26 @@
 package com.lavenberg;
 
-import java.util.Scanner;
-
 public class AddingDifferentTypes {
-	public static void main(String args[]) {
-	//Getting input from user
-	Scanner input = new Scanner (System.in);
-	System.out.println("Enter an int, float and double number seperated by spaces: ");
-	
-	int a = input.nextInt();
-	System.out.println("You entered integer" + a);
-	
-	double b = input.nextDouble(); 
-    System.out.println("You entered double " + b); 
-    
-    float c = input.nextFloat(); 
-    System.out.println("You entered float " + c);
-    //Printing number to the screen
-    System.out.println("\n");
-    System.out.println("Types Entered...");
-    System.out.println("    integer a = " + a);
-    System.out.println("    double b = " + b);
-    System.out.println("    float c = " + c);
+	public static void addTypes(int a, double b, float c) {
+	        	
+	        System.out.println("Variable values...");
+	        System.out.println("    int a = " + a);
+	        System.out.println("    double b = " + b);
+	        System.out.println("    float c = " + c);
 
-    //Adding numbers
-    System.out.println("\n");
-    System.out.println("Adding Types...");
-    System.out.println("    int a + float b = " + (a + b));
-    System.out.println("    double b + float c = " + (b + c));
-    System.out.println("    int a + double b + float c = " + (a + b + c));
-	
+	        //adding numbers
+	        System.out.println("\n");
+	        System.out.println("Adding Numbers...");
+	        System.out.println("    a + b = " + (a + b));
+	        System.out.println("    c + d = " + (b + c));
+	        System.out.println("    a + b + c = " + (a + b + c));
+	    }
+
+	    public static void main(String args[]) {
+	        int a = 7;
+	        double b = 12.5;
+	        float c = 2;
+	        addTypes(a, b, c);
+	    }
 	}
-}
+
