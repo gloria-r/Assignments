2b74b6c50f5540f0385b923a23e4caaacaa6d1d8
Gloria L <79482385+gloria-ss@users.noreply.github.com>
2021-02-28 23:04:26 -0500

    Add files via upload

---
+++ Week1/Wk1-Eval/SampleSingleton.java
@@ -0,0 +1,35 @@
+//Fixed Singleton
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SampleSingleton {
+	
+	private static Connection conn = null;
+	
+	private static SampleSingleton instance;
+	
+	public static SampleSingleton getInstance() {
+		if(instance == null)
+        {
+            instance = new SampleSingleton();
+        }
+		return instance;
+	}
+	
+	public static void databaseQuery(BigDecimal input) {
+		conn = DriverManager.getConnection("url of database");
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select id from table");
+		int x = 0;
+		while(rs.next()) {
+			x = rs.getInt(1) * input;
+		}
+	}
+}
+
+
+
